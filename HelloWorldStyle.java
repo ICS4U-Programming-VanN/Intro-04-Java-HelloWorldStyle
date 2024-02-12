96798a163236342900c6914e888063efb5720302
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2024-02-12 16:08:07 +0000

    Added linter fixes

--- HelloWorldStyle.java
+++ HelloWorldStyle.java
@@ -1,13 +1,22 @@
 /**
- * Displays Hello World message
+ * . Displays Hello World message.
  *
  * @author Van
  * @version 1.0
  * @since 2024/02/09
  */
 public class HelloWorldStyle {
+  /**
+   * Class constructor to satisfy the linter.
+   *
+   * @param none
+   */
+  public HelloWorldStyle() {}
+
   public static void main(String[] args) {
-    // Displays message
+    /**
+     * @param none
+     */
     System.out.println("Hello World (with style)!");
   }
 }
