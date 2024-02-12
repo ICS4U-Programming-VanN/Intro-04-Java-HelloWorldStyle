71799153f92e87de98dd9b5aeffb98a05085f2ae
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2024-02-12 03:12:52 +0000

    Added linter, created code for HelloWorldStyle.java, and ran linter

---
+++ HelloWorldStyle.java
@@ -0,0 +1,13 @@
+/**
+ * Displays Hello World message
+ *
+ * @author Van
+ * @version 1.0
+ * @since 2024/02/09
+ */
+public class HelloWorldStyle {
+  public static void main(String[] args) {
+    // Displays message
+    System.out.println("Hello World (with style)!");
+  }
+}
