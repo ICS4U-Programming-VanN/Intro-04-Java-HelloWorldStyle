cfa2e77e092dd1126ccb3e03a5d6a4956a854284
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2024-02-15 15:59:33 +0000

    linter ixexd

--- com/example/HelloWorldStyle.java
+++ com/example/HelloWorldStyle.java
@@ -1,6 +1,5 @@
-/**
- * . Displays Hello World.
- *
+/**.
+ * Displays Hello World.
  * @author Van
  * @version 1.0
  * @since 2024/02/09
@@ -8,22 +7,22 @@
 package com.example;
 
 public final class HelloWorldStyle {
-  /**
-   * This is a private constructor used to satisfy the linter.
-   *
-   * @exception IllegalStateException Utility class.
-   * @see IllegalStateException
-   */
-  private HelloWorldStyle() {
-    throw new IllegalStateException("Utility class");
-  }
+    /**
+     * This is a private constructor used to satisfy the linter.
+     * @exception IllegalStateException Utility class.
+     * @see IllegalStateException
+     */
+    private HelloWorldStyle() {
+        throw new IllegalStateException("Utility class");
+    }
 
-  /**
-   * Javadoc
-   *
-   * @param args Unused
-   */
-  public static void main(final String[] args) {
-    System.out.println("Hello World (with style)!");
-  }
+    /**.
+     * Displays Message.
+     *
+     * @param args Unused.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World (with style)!!");
+        System.out.println("Hello World (with style)!!");
+    }
 }
