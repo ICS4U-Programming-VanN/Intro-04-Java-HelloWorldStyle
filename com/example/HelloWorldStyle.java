3018d1524fea7455c01a7b01feee8c02c24cfde9
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2024-02-15 15:22:35 +0000

    Fixed some linter errors

--- com/example/HelloWorldStyle.java
+++ com/example/HelloWorldStyle.java
@@ -1,17 +1,24 @@
 package com.example;
-
 /**
  * @author Van
  * @version 1.0
  * @since 2024/02/09
  */
-public class HelloWorldStyle {
+public final class HelloWorldStyle {
+    /**
+     * This is a private constructor used to satisfy the linter.
+     * @exception IllegalStateException Utility class.
+     * @see IllegalStateException
+     */
+    private HelloWorldStyle() {
+        throw new IllegalStateException("Utility class");
+    }
   /**
    * Javadoc
    *
-   * @param null
+   * @param unused
    */
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     System.out.println("Hello World (with style)!");
   }
 }
