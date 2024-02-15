11ce78735807a5773f7d0e159b67abb7caadf188
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2024-02-15 15:10:45 +0000

    Added Linter Com

--- com/example/HelloWorldStyle.java
+++ com/example/HelloWorldStyle.java
@@ -1,22 +1,17 @@
+package com.example;
+
 /**
- * . Displays Hello World message.
- *
  * @author Van
  * @version 1.0
  * @since 2024/02/09
  */
 public class HelloWorldStyle {
   /**
-   * Class constructor to satisfy the linter.
+   * Javadoc
    *
-   * @param none
+   * @param null
    */
-  public HelloWorldStyle() {}
-
   public static void main(String[] args) {
-    /**
-     * @param none
-     */
     System.out.println("Hello World (with style)!");
   }
 }
